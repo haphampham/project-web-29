5ebb28181f09a11c08ea4e6dd453b5a76f05457f
phamha <phamhah150@gmail.com>
2023-01-10 16:21:09 +0700

    hhh

---
+++ trang chủ.js
@@ -0,0 +1,11 @@
+const rightbtn = document.querySelector('.fa-caret-right')
+const leftbtn = document.querySelector('.fa-caret-left')
+
+rightbtn.addEventListener("click", function(){
+    document.querySelector(".silder-conten-left-top").style.right ="100%"
+})
+leftbtn.addEventListener(".click",function(){
+    document.querySelector(".slider-conten-left-top").style.right ="0%"
+})
+
+
